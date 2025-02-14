2049d52499d23598bc8964b903dc2f11d0abcd9e
BaranGursoy <baran.guersoy@tum.de>
2025-02-14 14:13:17 +0000

    Added grid resolution as a compiler argument

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
+#include <cstdlib>
 #include "VoxelCarve.h"
 #include "VoxelGrid.h"
 #include "mesh/PostProcInterface.h"
 #include "sam_segmentation_generation.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    constexpr int DEFAULT_RESOLUTION = 300;
+    int resolution = DEFAULT_RESOLUTION;
+
+    if (argc > 1) {
+        try {
+            resolution = std::stoi(argv[1]); // Convert the argument to an integer
+        } catch (const std::exception& e) {
+            std::cerr << "Invalid argument: " << argv[1] << ". Using default resolution: " 
+                      << DEFAULT_RESOLUTION << std::endl;
+            resolution = DEFAULT_RESOLUTION;
+        }
+    }
+
+    std::cout << "Using resolution: " << resolution << std::endl;
+
     // Main Chad Segmentation part
     const SegmentationParams params;
     auto silhouettes = SamSegmentationGenerator::grabSegmentedImages(params);
 
     // Rest of the code
-    VoxelCarve voxelCarve(silhouettes, 500, true); // the number is the highest resolution for a dimension (others will be calculated automatically to create cubic voxels)
+    VoxelCarve voxelCarve(silhouettes, resolution, true); // the number is the highest resolution for a dimension (others will be calculated automatically to create cubic voxels)
     VoxelGrid carved_voxel_grid = voxelCarve.carve_voxel_grid();
 
     PostProcInterface postProcInterface;
